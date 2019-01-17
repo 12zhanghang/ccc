11150efb9f55b584b85ca0a4422dab7af38b79da
Your Name <you@example.com>
2019-01-17 20:00:08 +0800

    构建框架

--- main.c
+++ main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 
-int main(){
 
-    printf("hello world!");
+struct point
+
+{
+    int x;
+    int y;
+};
+
+int main(){
+    int index=0;
+    struct point arr[100];
+    printf("jin ru zhuan chang guan li xi tong\n");
+    while(1)
+    {
+        printf("1---tian jia che lian wei zi\n");
+        printf("2---shan chu\n");
+        printf("3---xian shi\n");
+        printf("4---fan hui li wo zui jin de che wei\n");
+        printf("5---tui chu\n");
+        printf("qinshuruxiangyinggongneng\n");
+        int code;
+        scanf("%d",&code); 
+        if(code==1)
+        {
+        
+        }
+        if(code==2)
+        {
+            
+        }
+        if(code==3)
+        {
+            
+        }
+        if(code==4)
+        {
+            
+        }
+        if(code==5)
+        {
+           printf("tui chu\n");
+           break;
+        }
+    }
 
     return 0;
 
